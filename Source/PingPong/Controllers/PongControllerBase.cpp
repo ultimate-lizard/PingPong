e64c4277e852b697f1bb21ff3cbe68123f25b612
Mykhailo Bekker <mykhailo.becker@gmail.com>
2024-09-20 18:18:05 +0400

    added controller base

---
+++ Source/PingPong/Controllers/PongControllerBase.cpp
@@ -0,0 +1,66 @@
+#include "Controllers/PongControllerBase.h"
+
+#include "Camera/CameraActor.h"
+#include "Net/UnrealNetwork.h"
+#include "GameFramework/PlayerState.h"
+
+APongControllerBase::APongControllerBase()
+{
+	PlayerCamera = nullptr;
+	PlayerIndex = 0;
+}
+
+void APongControllerBase::SetPlayerCamera(ACameraActor* CameraActor)
+{
+	PlayerCamera = CameraActor;
+}
+
+ACameraActor* APongControllerBase::GetPlayerCamera()
+{
+	return PlayerCamera;
+}
+
+void APongControllerBase::OnPossess(APawn* PossessedPawn)
+{
+	Super::OnPossess(PossessedPawn);
+
+	if (PlayerCamera)
+	{
+		SetViewTarget(PlayerCamera);
+	}
+}
+
+void APongControllerBase::ClientRestart_Implementation(APawn* NewPawn)
+{
+	Super::ClientRestart_Implementation(NewPawn);
+
+	if (PlayerCamera)
+	{
+		SetViewTarget(PlayerCamera);
+	}
+}
+
+void APongControllerBase::SetPlayerIndex(int32 Index)
+{
+	PlayerIndex = Index;
+}
+
+int32 APongControllerBase::GetPlayerIndex() const
+{
+	return PlayerIndex;
+}
+
+void APongControllerBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(APongControllerBase, PlayerCamera);
+}
+
+void APongControllerBase::OnRep_PlayerCamera()
+{
+	if (PlayerCamera && GetViewTarget() != PlayerCamera)
+	{
+		SetViewTarget(PlayerCamera);
+	}
+}
---
+++ Source/PingPong/Controllers/PongControllerBase.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+
+#include "PongControllerBase.generated.h"
+
+UCLASS()
+class PINGPONG_API APongControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	APongControllerBase();
+
+	void SetPlayerCamera(ACameraActor* CameraActor);
+	ACameraActor* GetPlayerCamera();
+
+	void SetPlayerIndex(int32 Index);
+	int32 GetPlayerIndex() const;
+
+	virtual void OnPossess(APawn* PossessedPawn) override;
+	virtual void ClientRestart_Implementation(APawn* NewPawn) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	UFUNCTION()
+	void OnRep_PlayerCamera();
+
+	UPROPERTY(ReplicatedUsing = OnRep_PlayerCamera)
+	ACameraActor* PlayerCamera;
+
+	int32 PlayerIndex;
+};
